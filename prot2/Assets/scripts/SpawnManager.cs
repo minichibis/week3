d46647f79c8a61e10f6c98615a71881936d91aa2
minichibis <minichibis@gmail.com>
2020-09-11 16:15:59 -0500

    ugh im too tired for more i need a break

--- prot2/Assets/scripts/SpawnManager.cs
+++ prot2/Assets/scripts/SpawnManager.cs
@@ -15,10 +15,15 @@ public class SpawnManager : MonoBehaviour
     void Update() {
         timer--;
 		if(timer <= 0){
-			Vector3 v = new Vector3(Random.Range(-14, 14), 0, 20);
-			Quaternion r = new Quaternion(0, 180, 0, 0);
-			Instantiate(spawnables[Random.Range(0, spawnables.Length)], v, r);
 			timer = Random.Range(10, 40);
+			SpawnDude();
 		}
     }
+	
+	void SpawnDude(){
+		Vector3 v = new Vector3(Random.Range(-14, 14), 0, 20);
+		Quaternion r = new Quaternion(0, 180, 0, 0);
+		GameObject dude = spawnables[Random.Range(0, spawnables.Length)];
+		Instantiate(dude, v, r);
+	}
 }
