62b111b86e3440c3c810775f085f99580790dd8c
minichibis <minichibis@gmail.com>
2020-09-11 15:33:28 -0500

    we have given the player a hammer, now all we need are the nails

---
+++ prot2/Assets/scripts/GoGoGo.cs
@@ -0,0 +1,18 @@
+﻿/** Sam Carpenter
+* Prototype 2
+* Makes various things move around
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoGoGo : MonoBehaviour
+{
+	public float speed = 40;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
---
+++ prot2/Assets/scripts/PlayerKillinging.cs
@@ -0,0 +1,18 @@
+﻿/** Sam Carpenter
+* Prototype 2
+* Blesses the player with the power to smite down any entity so foolish as to exist.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKillinging : MonoBehaviour{
+	public GameObject projectile;
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+			Instantiate(projectile, transform.position, transform.rotation);
+		}
+    }
+}
