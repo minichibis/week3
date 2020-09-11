2ebf879e222b1b0a8cfcad82412ef963bcd2927d
minichibis <minichibis@gmail.com>
2020-09-11 15:06:53 -0500

    player moves around watched by 3 demons

---
+++ prot2/Assets/scripts/PlayerMoveing.cs
@@ -0,0 +1,24 @@
+﻿/** Sam Carpenter
+* Prototype 2
+* Makes the player move around on one axis, clamped into a box
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveing : MonoBehaviour
+{
+    public float hInput;
+	public float maxdist = 0;
+	public float hspeed = 0;
+	private float c = 0;
+	
+
+
+    void Update(){
+		hInput = Input.GetAxis("Horizontal");
+		c = Mathf.Clamp(transform.position.x + Time.deltaTime * hspeed * hInput, maxdist * -1, maxdist);
+		transform.position = new Vector3(c, 0, 0);
+    }
+}
